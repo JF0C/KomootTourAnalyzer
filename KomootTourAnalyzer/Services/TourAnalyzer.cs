20b5220ca53afc11b7be7715770b8918758faff3
Jan Chen <jan_201@hotmail.de>
2024-07-26 23:17:22 +0200

    year summaries

--- KomootTourAnalyzer/Services/TourAnalyzer.cs
+++ KomootTourAnalyzer/Services/TourAnalyzer.cs
@@ -7,7 +7,7 @@ public class TourAnalyzer : ITourAnalyzer
     private readonly Action<string> logger = Console.WriteLine;
     private readonly ITourLoader tourLoader;
     private TourSummary? tourSummary;
-
+    private Dictionary<int, TourSummary> summariesByYears = [];
     public TourAnalyzer(ITourLoader tourLoader, Action<string>? logger){
         this.tourLoader = tourLoader;
         this.logger = logger ?? this.logger;
@@ -38,14 +38,41 @@ public class TourAnalyzer : ITourAnalyzer
             s.EndDate = s.EndDate < e.Date ? e.Date : s.EndDate;
             return s;
         });
+        foreach (var tour in tours)
+        {
+            if (!summariesByYears.ContainsKey(tour.Date.Year)){
+                summariesByYears[tour.Date.Year] = new();
+            }
+            var s = summariesByYears[tour.Date.Year];
+            s.DistanceInMeters += tour.DistanceInMeters;
+            s.ElevationDownInMeters += tour.ElevationDownInMeters;
+            s.ElevationUpInMeters += tour.ElevationUpInMeters;
+            s.SecondsInMotion += tour.SecondsInMotion;
+            s.SecondsTotal += tour.SecondsTotal;
+            s.Date = s.Date > tour.Date ? tour.Date : s.Date;
+            s.EndDate = s.EndDate < tour.Date ? tour.Date : s.EndDate;
+        }
     }
 
     public void Print()
     {
+        logger("");
+        PrintSelection(tourSummary);
+        foreach (var k in summariesByYears.Keys.Order())
+        {
+            var v = summariesByYears[k];
+            logger("");
+            logger(TimePrefix() + "Year " + k);
+            PrintSelection(v);
+        }
+    }
+
+    private void PrintSelection(TourSummary? tourSummary){
         logger(TimePrefix() + "Total distance [km]:         " + (tourSummary?.DistanceInMeters / 1000.0 ?? 0).ToString("0.00"));
         logger(TimePrefix() + "Total elevation up [km]:     " + (tourSummary?.ElevationUpInMeters / 1000.0 ?? 0).ToString("0.00"));
         logger(TimePrefix() + "Total elevation down [km]:   " + (tourSummary?.ElevationDownInMeters / 1000.0 ?? 0).ToString("0.00"));
         logger(TimePrefix() + "Total time in motion:        " + TimeSpan.FromSeconds(tourSummary?.SecondsInMotion ?? 0));
+        logger(TimePrefix() + "Average velocity [km/h]:     " + (tourSummary?.DistanceInMeters / 1000.0 ?? 0 / tourSummary?.SecondsInMotion ?? 1 * 3600).ToString("0.00"));
         logger(TimePrefix() + "First Tour [day.month.year]: " + tourSummary?.Date.ToString("dd.MM.yyyy") ?? "error");
         logger(TimePrefix() + "Last Tour [day.month.year]:  " + tourSummary?.EndDate.ToString("dd.MM.yyyy") ?? "error");
     }
