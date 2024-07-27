863057ad36ef82531e2297ad75970639fc21b5a2
Jan Chen <jan_201@hotmail.de>
2024-07-27 02:06:34 +0200

    cleanup

--- KomootTourAnalyzer/Program.cs
+++ KomootTourAnalyzer/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using KomootTourAnalyzer;
-using KomootTourAnalyzer.Services;
+﻿using KomootTourAnalyzer;
 using Microsoft.Extensions.Configuration;
 
 Console.WriteLine("Starting Tour Analyzer");
